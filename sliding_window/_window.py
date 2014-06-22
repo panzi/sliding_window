c1cab546829675388ceec307153299bf3abd9e2e
Mathias Panzenböck <grosser.meister.morti@gmx.net>
2014-06-22 19:05:36 +0200

    added param. default=None and only export "window"

--- sliding_window/_window.py
+++ sliding_window/_window.py
@@ -1,14 +1,16 @@
 from collections import deque
 
+__all__ = ['window']
+
 # Python 2 compatibility
 try:
     xrange
 except NameError:
     xrange = range
 
-def window(seq, n=2):
+def window(seq, n=2, default=None):
     it = iter(seq)
-    win = deque((next(it, None) for _ in xrange(n)), maxlen=n)
+    win = deque((next(it, default) for _ in xrange(n)), maxlen=n)
     yield tuple(win)
     append = win.append
     for e in it:
